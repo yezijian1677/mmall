25e5b3a28dec147f30b28450a75311b2563f64d7
982_100005167237_yezijian1677 <982_100005167237_yezijian1677@git.cloud.tencent.com>
2019-01-28 16:46:35 +0800

    complete 75percent

--- src/main/java/com/mmall/service/ICategoryService.java
+++ src/main/java/com/mmall/service/ICategoryService.java
@@ -7,7 +7,10 @@ import java.util.List;
 
 public interface ICategoryService {
     ServerResponse addCategory(String categoryName, Integer parentId);
+
     ServerResponse updateCategoryName(Integer categoryId, String categroyName);
+
     ServerResponse<List<Category>> getChildrenParallelCategory(Integer categoryId);
-    ServerResponse selectCategoryAndChildrenById(Integer categoryId);
+
+    ServerResponse<List<Integer>> selectCategoryAndChildrenById(Integer categoryId);
 }
